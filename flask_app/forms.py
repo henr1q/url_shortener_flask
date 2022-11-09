5cfff5f046632da3b32485ca7e429e66167b60d8
henr1q <69945767+henr1q@users.noreply.github.com>
2022-11-09 06:35:40 -0300

    form typo

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_wtf.file import FileField, FileAllowed
 
-class UrlForm(FlaskForm):
 
-    url = StringField("URL", validators=[DataRequired(), URL()])
-    submit = SubmitField("OK")
+class UrlForm(FlaskForm):
+    url = StringField("URL", validators=[DataRequired()])
+    submit = SubmitField('Shorten URL')
